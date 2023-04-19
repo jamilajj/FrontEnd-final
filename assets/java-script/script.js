58b85f51aef95bfa9843e47f222e73bad9ade842
Jamila J. Jumshudova <jamilajj@code.local>
2023-04-19 18:56:56 +0400

    nav2

--- assets/java-script/script.js
+++ assets/java-script/script.js
@@ -1,1 +1,16 @@
 console.log("salam");
+
+let btn = document.querySelector(".all_categories_btn");
+let ddd = document.querySelector(".all_categories_list");
+let list= document.querySelector(".header_section2");
+btn.addEventListener("mouseover", function(){
+    ddd.classList.remove("d-none");
+})
+
+list.addEventListener("mouseout", function(){
+    ddd.classList.add("d-none");
+})
+let hov_li=document.querySelectorAll(".all_categories_btn.li");
+// hov_li.addEventListener("mouseover",function(){
+//     hov_li.classList.add(" background-color: blueviolet;");
+// })
